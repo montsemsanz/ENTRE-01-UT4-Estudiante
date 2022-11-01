84104bc8abdc154f80cb5c347d08b23bcf4f9438
Aitor Zubillaga <azubillagasdam1@gmail.com>
2022-11-01 19:44:08 +0100

    Segunda parte del metodo Main

--- TestEstudiante.java
+++ TestEstudiante.java
@@ -9,10 +9,16 @@ public class TestEstudiante {
      */
     public static void main(String[] args) {
         Estudiante estudiante1 = new Estudiante ("MOLINA HUARTE, Isabel");
-        UnidadTrabajo InteraccionDeObjetos = new UnidadTrabajo ("Interacción de objetos. Estructura de control iterativa", 15, 11, 2022,55, 60, 10, 30);
-        NotaEstudianteUnidad notaUnidad1 = new NotaEstudianteUnidad (InteraccionDeObjetos, 5, 6, 7);
+        UnidadTrabajo unidad1 = new UnidadTrabajo ("Interacción de objetos. Estructura de control iterativa", 15, 11, 2022,55, 60, 10, 30);
+        NotaEstudianteUnidad notaUnidad1 = new NotaEstudianteUnidad (unidad1, 5, 6, 7);
         estudiante1.registrarNotaUnidad(notaUnidad1);
         estudiante1.print();
+        
+        
+        Fecha fecha1 = new Fecha(21, 9, 2022);
+        PonderacionInstrumentos ponderacion1 = new PonderacionInstrumentos(6, 10, 7);
+        UnidadTrabajo unidad2 = new UnidadTrabajo("Objetos y clases",fecha1,10, ponderacion1);
+        NotaEstudianteUnidad notaUnidad2 = new NotaEstudianteUnidad (unidad2, 6, 10, 7);
        
     }
 }
