f8fd1a0253a73c1c4188d6962441e21542eb879a
Elais Lahmami <elahmamihdam1@gmail.com>
2022-11-01 13:26:38 +0100

    Constructor hecho

--- UnidadTrabajo.java
+++ UnidadTrabajo.java
@@ -16,9 +16,10 @@ public class UnidadTrabajo {
      */
     public UnidadTrabajo(String nombre, int dia, int mes, int year, int peso,
                          int controles, int actividades, int proyectos) {
-       //TODO
-       
-       
+        this.nombre = nombre;
+        this.fechaFin = new Fecha(dia, mes, year);
+        this.pesoUnidad= peso;
+        this.ponderacion = new PonderacionInstrumentos(controles, actividades, proyectos);
     }
 
     /**
