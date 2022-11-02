ac4e617c4053770468bc6b51f02c30622cfd1b69
Cristina López <clopezldam1@gmail.com>
2022-11-02 16:08:21 +0100

    Modificado método calcularNotaUnidad() de la clase NotaEstudianteUnidad.

--- NotaEstudianteUnidad.java
+++ NotaEstudianteUnidad.java
@@ -86,10 +86,14 @@ public class NotaEstudianteUnidad {
      */
     public double calcularNotaUnidad() {
          
+        //controles = 6.00/10 y controles valen 70/100, por lo que si 10 es 70, 6 es x: x=6*70/10 
+        //x es la nota/100 de la unidad total y nosotros queremos que la nota final esté escrita en nota/10x
          
-         double ponderaControles = unidad.getPonderacion().getControles();
-         
-         return 0;
+        double notaPonderadaControles = getNotaControles() * (unidad.getPonderacion().getControles()) / 10;
+        double notaPonderadaActividades = getNotaActividades() * (unidad.getPonderacion().getActividades()) / 10;
+        double notaPonderadaProyectos = getNotaProyectos() * (unidad.getPonderacion().getProyectos()) / 10;
+
+        return (notaPonderadaControles + notaPonderadaActividades + notaPonderadaProyectos) / 10;
 
 
     }
